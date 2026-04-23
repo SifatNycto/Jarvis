27281ed7caae9896a90f3dff60e990ee0785f902
SifatNycto <sftmhd@gmail.com>
2026-04-23 23:42:31 +0600

    added some initial files

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-print("hello jarvis")
-print("hello worls")
+from core.listen import record_audio
+from core.transcribe import transcribe_audio
+
+
+def main():
+    print("JARVIS is listening...")
+
+    audio_file = record_audio(duration=5)
+
+    if not audio_file:
+        print("⚠️ No usable audio")
+        return
+
+    text = transcribe_audio(audio_file)
+
+    if text:
+        print("✅ Transcription complete")
+    else:
+        print("⚠️ Could not understand audio")
+
+
+if __name__ == "__main__":
+    main()
